a5b68557352286d6af341d9f48db9b22d08c8214
ConanYu <C.zing@outlook.com>
2025-03-06 17:36:25 +0800

    feat: add long text dialog

--- src/interaction.rs
+++ src/interaction.rs
@@ -1,9 +1,13 @@
+use crate::{
+    common::{set_body_overflow_style, value_length, CopyButton},
+    css::*,
+    JsonViewer, JsonViewerOption,
+};
 use gloo::utils::{document, window};
 use serde_json::Value;
 use wasm_bindgen::prelude::*;
 use yew::prelude::*;
 use yew::virtual_dom::VNode;
-use crate::{common::{set_body_overflow_style, value_length, CopyButton}, css::*, JsonViewer, JsonViewerOption};
 
 #[derive(PartialEq, Properties)]
 struct DialogProps {
@@ -44,19 +48,33 @@ fn dialog(props: &DialogProps) -> Html {
                 }
             };
             let keydown_handler = Closure::<dyn Fn(KeyboardEvent)>::wrap(Box::new(keydown_handler));
-            window().add_event_listener_with_callback("keydown", keydown_handler.as_ref().unchecked_ref()).unwrap();
+            window()
+                .add_event_listener_with_callback(
+                    "keydown",
+                    keydown_handler.as_ref().unchecked_ref(),
+                )
+                .unwrap();
             move || {
                 if index == 0 {
                     set_body_overflow_style("auto");
                 }
-                window().remove_event_listener_with_callback("keydown", keydown_handler.as_ref().unchecked_ref()).unwrap();
+                window()
+                    .remove_event_listener_with_callback(
+                        "keydown",
+                        keydown_handler.as_ref().unchecked_ref(),
+                    )
+                    .unwrap();
             }
         }
     });
     let onclick_dialog = Callback::from(|e: MouseEvent| {
         e.stop_propagation();
     });
-    let overlay_css = if index == &0 { "" } else { "background-color: rgba(0, 0, 0, 0.4);" };
+    let overlay_css = if index == &0 {
+        ""
+    } else {
+        "background-color: rgba(0, 0, 0, 0.4);"
+    };
     html! {
         <div class={classes!(OVERLAY_CSS.as_str(), "yew_json_viewer_dialog")} style={overlay_css}
              data-index={index.to_string()} onclick={set_close.clone()}>
@@ -77,7 +95,9 @@ fn button_control_dialog_json_viewer(props: &ButtonControlDialogJsonViewerProps)
     let ButtonControlDialogJsonViewerProps { value } = props;
     let open = use_state(|| false);
     let mut option: JsonViewerOption = Default::default();
-    let index = document().get_elements_by_class_name("yew_json_viewer_dialog").length() as i32;
+    let index = document()
+        .get_elements_by_class_name("yew_json_viewer_dialog")
+        .length() as i32;
     option.dialog_index = Some(index);
     let viewer = JsonViewer::new_with_option(value.clone(), option);
     let set_close = {
@@ -110,6 +130,47 @@ fn button_control_dialog_json_viewer(props: &ButtonControlDialogJsonViewerProps)
     }
 }
 
+#[derive(PartialEq, Properties)]
+struct LongTextViewerProps {
+    text: String,
+}
+
+#[function_component(LongTextViewer)]
+fn long_text_viewer(props: &LongTextViewerProps) -> Html {
+    let open = use_state(|| false);
+    let LongTextViewerProps { text } = props;
+    let onclick = Callback::from({
+        let open = open.clone();
+        move |_| {
+            open.set(true);
+        }
+    });
+    let set_close = {
+        let open = open.clone();
+        Callback::from(move |_| {
+            open.set(false);
+        })
+    };
+    html! {
+        <>
+            <span style="cursor: pointer;" {onclick}>
+                <div class={INTERACTION_BUTTON_CSS.as_str()}/>
+            </span>
+            if *open {
+                <Dialog {set_close}>
+                    <span class="h5">{"Long Text"}</span>
+                    <span style="vertical-align: 0.2em; margin-left: 0.2em;">
+                        <CopyButton text={text.to_string()}/>
+                    </span>
+                    <pre class={JSON_DOCUMENT.as_str()} style="white-space: pre-wrap; max-height: 80vh;">
+                        {text}
+                    </pre>
+                </Dialog>
+            }
+        </>
+    }
+}
+
 #[wasm_bindgen(inline_js = r#"
 export function with_global_javascript_interaction() {
     return !!window.__yew_json_viewer__interaction;
@@ -129,13 +190,11 @@ pub fn default_interaction(arg: &Value) -> Vec<VNode> {
             let onclick = Callback::from(move |_: MouseEvent| {
                 func.call0(&JsValue::NULL).unwrap();
             });
-            return vec![
-                html! {
-                    <span style="cursor: pointer;" {onclick}>
-                        <div class={INTERACTION_BUTTON_CSS.as_str()}/>
-                    </span>
-                }
-            ];
+            return vec![html! {
+                <span style="cursor: pointer;" {onclick}>
+                    <div class={INTERACTION_BUTTON_CSS.as_str()}/>
+                </span>
+            }];
         }
     }
     let mut result = vec![];
@@ -144,7 +203,9 @@ pub fn default_interaction(arg: &Value) -> Vec<VNode> {
             if let Ok(url) = url::Url::parse(s.as_str()) {
                 if ["http", "https", "ftp", "ftps"].contains(&url.scheme()) {
                     let onclick = Callback::from(move |_: MouseEvent| {
-                        window().open_with_url_and_target(url.as_str(), "_blank").unwrap();
+                        window()
+                            .open_with_url_and_target(url.as_str(), "_blank")
+                            .unwrap();
                     });
                     result.push(html! {
                         <span style="cursor: pointer;" {onclick}>
@@ -161,6 +222,10 @@ pub fn default_interaction(arg: &Value) -> Vec<VNode> {
                     }
                     _ => {}
                 }
+            } else if s.len() > 100 {
+                result.push(html! {
+                    <LongTextViewer text={s.clone()}/>
+                });
             }
         }
         Value::Array(_) | Value::Object(_) => {
